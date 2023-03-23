c8bf5dd1c35687890705050a778c562a250dba28
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-03-23 07:14:56 -0500

    Ajustes para Deploy en Render

--- config/database.js
+++ config/database.js
@@ -3,11 +3,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env(
+        "DATABASE_HOST",
+        "dpg-cge3rdfekgjpv7uqlfjg-a.oregon-postgres.render.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "guitarla-strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "admin"),
+      database: env("DATABASE_NAME", "guitarla_qaso"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "0pZFx2lupTpGO6L5kTlKzyqSQ9O3n5jI"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       // ssl: {
       //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
